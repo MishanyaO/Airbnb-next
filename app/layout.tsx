6de0a00318c316e9ac97658a5ebeec8f8f3c6a7d
Mikhail <mikhail.olianenko@gmail.com>
2023-07-13 13:40:07 +0300

    Auth UI

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import ClientOnly from "./components/ClientOnly";
+import Navbar from "./components/Navbar/Navbar";
+import RegisterModal from "./components/modals/RegisterModal";
+import "./globals.css";
+
+const font = Nunito({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
+        {children}
+      </body>
+    </html>
+  );
+}
